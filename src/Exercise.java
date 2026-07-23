0191cd60d3962621bdf35ddd1a5e870e2c0482b0
magdyelmansy4 <magdyelmansy4@gmail.com>
2026-07-23 22:30:08 +0300

    week 1 exercises

--- src/Exercise.java
+++ src/Exercise.java
@@ -22,7 +22,6 @@ class Manager extends Employee {
     }
 
 
-
     public String getDetails() {
         return super.getDetails() + ", Department: " + department;
     }
@@ -40,15 +39,107 @@ public class Exercise {
         for (int num : numbers) {
 
 
-            if (num > largest)
-                largest = num;
+            if (num > largest) largest = num;
 
-            if (num < smallest)
-                smallest = num;
+            if (num < smallest) smallest = num;
         }
 
         System.out.println("Smallest: " + smallest);
         System.out.println("Largest: " + largest);
+
+        System.out.println("______ Exercise 3 _______");
+
+        int score = 75;
+        char grade;
+
+        // Pass/Fail check
+        if (score >= 50) {
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+
+        // Grade assignment
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 75) {
+            grade = 'B';
+        } else if (score >= 60) {
+            grade = 'C';
+        } else {
+            grade = 'D';
+        }
+
+        System.out.println("Grade: " + grade);
+
+        System.out.println("______ Exercise 4 _______");
+
+        // For loop
+        System.out.print("For Loop: ");
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i);
+            if (i < 5) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+
+        // While loop
+        System.out.print("While Loop: ");
+        int j = 1;
+        while (j <= 5) {
+            System.out.print(j);
+            if (j < 5) {
+                System.out.print(" ");
+            }
+            j++;
+        }
+        System.out.println();
+
+        // Do-while loop
+        System.out.print("Do-While Loop: ");
+        int k = 1;
+        do {
+            System.out.print(k);
+            if (k < 5) {
+                System.out.print(" ");
+            }
+            k++;
+        } while (k <= 5);
+
+        System.out.println();
+
+        System.out.println("______ Exercise 5 _______");
+
+        double num1 = 7;
+        double num2 = 3;
+        char operator = '+';
+
+        String again = "y";
+
+        while (again.equals("y")) {
+            if (operator == '+') {
+                double result = num1 + num2;
+                System.out.println("Result: " + result);
+            } else if (operator == '-') {
+                double result = num1 - num2;
+                System.out.println("Result: " + result);
+            } else if (operator == '*') {
+                double result = num1 * num2;
+                System.out.println("Result: " + result);
+            } else if (operator == '/') {
+                if (num2 == 0) {
+                    System.out.println("Cannot divide by zero.");
+                } else {
+                    double result = num1 / num2;
+                    System.out.println("Result: " + result);
+                }
+            }
+
+            again = "n";
+        }
+
+        System.out.println("Thank you for using the calculator.");
 
         System.out.println("________ Final Exercise ___________");
 
